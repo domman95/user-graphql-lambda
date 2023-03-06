3dbc9a3da631c6e7ecb6aec88dc4fc51d7999148
Dominik Lasek <61436262+domman95@users.noreply.github.com>
2023-03-06 19:02:08 +0100

    add EventStore and connection to EventStoreDB

--- src/infrastructure/lib/infrastructure-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
-export declare class InfrastructureStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps);
-}
